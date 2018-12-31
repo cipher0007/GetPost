ec6b1e452d0a086be4ce6b0ffdcb8ead3d875312
cipher0007 <cipher0007@BHUMI>
2018-12-31 02:06:35 -0800

    message set

---
+++ GetPost/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPost
+{
+    public class LoginModel
+    {
+        [JsonProperty("error")]
+        public string error { get; set; }
+        [JsonProperty("message")]
+        public string message { get; set; }
+    }
+}
--- GetPost/MainWindow.xaml.cs
+++ GetPost/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +22,56 @@ namespace GetPost
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
-        {
-            InitializeComponent();
+       
+            public MainWindow()
+            {
+                InitializeComponent();
+                PostRequest("http://easyscooter.co.in/android/test.php");
+            }
+            async void GetRequest(string url)
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage responseMessage = await client.GetAsync(url))
+                    {
+                        using (HttpContent content = responseMessage.Content)
+                        {
+                            string cont = await content.ReadAsStringAsync();
+                            mytext.Text = cont;
+                        }
+                    }
+                }
+            }
+
+            async void PostRequest(string url)
+            {
+                IEnumerable<KeyValuePair<string, string>> queries = new List<KeyValuePair<string, string>>()
+            {
+                new KeyValuePair<string, string>("fname","qwe"),
+                new KeyValuePair<string, string>("lname","aaaa"),
+                new KeyValuePair<string, string>("email","aa@gmail.com"),
+                new KeyValuePair<string, string>("number","12321231"),
+                new KeyValuePair<string, string>("password","1231322"),
+                new KeyValuePair<string, string>("fileToUpload","1232332"),
+                new KeyValuePair<string, string>("fileToUpload1","3213")
+            };
+
+                HttpContent httpContent = new FormUrlEncodedContent(queries);
+                using (HttpClient client = new HttpClient())
+                {
+                    using (HttpResponseMessage responseMessage = await client.PostAsync(url, httpContent))
+                    {
+                        using (HttpContent content = responseMessage.Content)
+                        {
+                            string cont = await content.ReadAsStringAsync();
+                            LoginModel loginModel = JsonConvert.DeserializeObject<LoginModel>(cont);
+
+                        //setting message
+                            mytext.Text = loginModel.message;
+                        }
+                    }
+                }
+            }
+
         }
     }
-}
